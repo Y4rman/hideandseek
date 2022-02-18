14560fc1b5b99374c66312bca9ad7620cba475c8
Y4rman Gaming <yarosvetbobr@gmail.com>
2022-02-18 21:27:51 +0300

    init commit

---
+++ hideandseek/hider.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hideandseek
+{
+    internal class hider
+    { 
+        private int damage = 1
+        private int health = 10
+        private int speed = 1
+    }
+}
