caa86500027db471fe370c10c6951ef82cd31012
Dan <dan@dminc.ru>
2022-02-19 08:21:42 +0300

    Создан класс игрового мира

---
+++ hideandseek/World.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace hideandseek
+{
+    internal class World
+    {
+        private int damage = 5;
+
+        public void MissDamage()
+        {
+            return damage;
+        }
+    }
+}
